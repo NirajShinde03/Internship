df525eebcd15975b6239179d475c528c5695d997
shindeniraj <131136644+shindeniraj@users.noreply.github.com>
2023-12-20 10:06:16 +0530

    modifieddatatypes

--- datatypes.js
+++ datatypes.js
@@ -1,4 +1,4 @@
-const score=100;
+ const score=100;
 const scoreValue =100.2;
 const isLoggedin =false;
 const outSidetemp =null;
@@ -8,3 +8,21 @@ console.log(userEmail);
 const id = Symbol('123');
 const anotherId =Symbol('123');
 console.log(id == anotherId);
+const bigNumber =123456789n;
+console.log(bigNumber);
+
+
+//nonprimitive datatypes
+
+const friends = ["Niraj","Om","sid"];
+let myObj={
+     name:"Hitesh",
+     age:22,
+     marks:89
+}
+
+const myFunction = function(){
+     console.log("Hello World");
+}
+console.log(typeof anotherId);
+console.log(typeof score);
