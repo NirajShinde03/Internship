a7f1732ceb7e391fc2f9ba6a3e0f88dd42e86b8a
shindeniraj <131136644+shindeniraj@users.noreply.github.com>
2023-12-20 09:36:27 +0530

    new file added

---
+++ variables.js
@@ -0,0 +1,20 @@
+/* let studName ="Niraj";
+
+// console.log("Student name is:",studName);
+// console.log(`student name is: ${studName}`);
+
+const pie =3.145;
+// console.log(pie); */
+
+
+const accountId =14453;
+let accountEmail = "shindeniraj084@gmail.com";
+var accountPass ="12345";
+let accountCity = "Pune";
+// let accountState;
+
+// accountId=2;
+// accountEmail="adddvdv@fdfd";
+// accountCity="nashik";
+
+console.table([accountId,accountEmail,accountPass,accountCity]);
